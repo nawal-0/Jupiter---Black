26e963a712426b2c0089dd5d4ea6604dfc0097fa
Nawal <nawalnazim@gmail.com>
2024-04-29 23:50:20 +1000

    added website header

---
+++ mycode/mylib/webapp/src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderFooter = () => {
+  return (
+    <>
+    <header className="page-header">
+      <h2>Jupiter - Black</h2>
+    </header>
+
+    <footer className="page-footer"></footer>
+    </>
+  );
+}
+
+export default HeaderFooter;
---
+++ mycode/mylib/webapp/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Home() {
+    return (
+        <div>
+        <main>
+        <h1 className='title'>Home</h1>
+        </main>
+        </div>
+    );
+}
